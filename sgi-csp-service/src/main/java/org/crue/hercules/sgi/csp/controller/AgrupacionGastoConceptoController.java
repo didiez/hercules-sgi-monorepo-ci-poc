1c7e06d30e5c47e812b542568b839fbe983ecf7c
Diego Díez Ricondo <diegodiez.ddr@gmail.com>
2025-03-31 21:07:39 +0200

    feat: prueba rápida para release-please

--- sgi-csp-service/src/main/java/org/crue/hercules/sgi/csp/controller/AgrupacionGastoConceptoController.java
+++ sgi-csp-service/src/main/java/org/crue/hercules/sgi/csp/controller/AgrupacionGastoConceptoController.java
@@ -61,6 +61,7 @@ public class AgrupacionGastoConceptoController {
   public ResponseEntity<AgrupacionGastoConceptoOutput> create(
       @Valid @RequestBody AgrupacionGastoConceptoInput agrupacionGastoConcepto) {
     log.debug("create(AgrupacionGastoConcepto agrupacionGastoConcepto) - start");
+    log.debug("prueba rápida!");
     AgrupacionGastoConcepto returnValue = service.create(convert(agrupacionGastoConcepto));
     log.debug("create(AgrupacionGastoConcepto agrupacionGastoConcepto) - end");
     return new ResponseEntity<>(convert(returnValue), HttpStatus.CREATED);
